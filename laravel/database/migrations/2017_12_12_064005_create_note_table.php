a260589f2272e09cc712848e416eb13348b28237
OliaVM <oliamo2016@gmail.com>
2017-12-12 16:07:03 +0700

    settings for connection with database, MODEL, VIEW, CONTROLLER and TABLE for NOTES

---
+++ laravel/database/migrations/2017_12_12_064005_create_note_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNoteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user_id');
+            $table->string('note_date');
+            $table->string('note_title');
+            $table->string('note_text');
+            //$table->string('new');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+}
